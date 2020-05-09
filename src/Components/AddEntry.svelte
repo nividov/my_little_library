eb457d988787efbf396b65f9e3e5d42717a07fa4
nividov <nividov@outlook.com>
2020-05-09 13:11:04 +0200

    dataHandling now happens externally

--- src/Components/AddEntry.svelte
+++ src/Components/AddEntry.svelte
@@ -1,41 +1,16 @@
 <script>
-    import { addEntry } from "./DataHandling.js"
-    import { data } from "../store.js"
+    import { addNewEntry } from "./DataHandling.js" 
     import HomeButton from "./HomeButton.svelte"
-
-    data.useLocalStorage();
-
-    let inputField
-
-    function newEntry(newThing){
-        let form = newThing.currentTarget
-        let title = form.elements.namedItem("title").value
-        let author = form.elements.namedItem("author").value
-
-        for(let i = 0; i < $data.length; i++){
-            if($data[i].title === title && $data[i].author === author ){
-                alert("Dieser Eintrag existiert bereits")
-                return
-            }
-        }
-
-        let newObject = {
-            title: title,
-            author: author
-        }
-        $data = [...$data, newObject] 
-        console.log($data)
-    }
 
 </script>
 
 <HomeButton />
 
-<div>Add Entry</div>
+<div>Add a new entry</div>
 
-<form on:submit|preventDefault={newEntry} name="form">
+<form on:submit|preventDefault={addNewEntry} name="form">
     <label for="title"> Titel </label>
-    <input type="text" name="title" bind:value={inputField}><br>
+    <input type="text" name="title"><br>
     <label for="author"> Autor </label>
     <input type="text" name="author"><br>
     <button type="submit">Go</button>
--- src/Components/DataHandling.js
+++ src/Components/DataHandling.js
@@ -1,6 +1,35 @@
 
-    export const addEntry = (arr, newTitle) => {
-        arr = [...arr, newTitle] 
-        console.log(arr)
+export function addNewEntry(newEntry) {
+    let newObject = processInput(newEntry)
+    checkForDuplicates(newObject)
+    if(!checkForDuplicates(newObject)){
+        addInputToData(newObject)
     }
+}
 
+function processInput(input){
+    let form = input.currentTarget
+    let title = form.elements.namedItem("title").value
+    let author = form.elements.namedItem("author").value    
+    
+    return {
+        title: title,
+        author: author
+    }
+}
+
+function checkForDuplicates(input){
+    let data = JSON.parse(localStorage.getItem("data"));
+    for(let i = 0; i < data.length; i++){
+        if(data[i] === input){
+            alert("Dieser Eintrag existiert bereits")
+            return true
+        }
+    }
+}
+
+function addInputToData(newEntry){
+    let data = JSON.parse(localStorage.getItem("data"));
+    data = [...data, newEntry]
+    localStorage.setItem("data", JSON.stringify(data))
+}
